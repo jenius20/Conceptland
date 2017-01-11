f39b03c7752669db07038456b0eaf0739a0fbf70
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-11 22:54:42 +0000

    Volume 2 - moving textures on floor

---
+++ Assets/Scripts/AnimateTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTexture : MonoBehaviour
+{
+    public Vector2 speed = Vector2.one;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset += speed * Time.deltaTime;
+
+	}
+}
