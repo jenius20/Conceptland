cf6ec65556c3f0bec64e5364e83a8e9420710309
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-11 01:01:26 +0000

    Enemy Exploding + multiple enemys

---
+++ Assets/Scripts/GibOnHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GibOnHit : MonoBehaviour
+{
+    public GameObject gib;
+
+    void OnTriggerEnter()
+    {
+        Instantiate(gib, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }   		
+}
