ff8c13dca360739811282805f75254703e10898b
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-12 17:39:20 +0000

    Volume 2 - Texture not animates forward and left/right

---
+++ Assets/Scripts/PseudoMoveLeftAndRight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoMoveLeftAndRight : MonoBehaviour
+{
+    public Vector2 speed = Vector2.one;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    }
+}
