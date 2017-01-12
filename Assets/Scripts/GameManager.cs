b4e7e8efc4238ea5008a1279297dc137dbd7325d
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-12 23:38:49 +0000

    Gameover reset - dont know what gui does or if it works?

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameOver = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); ;
+            }
+        }
+		
+	}
+
+    void OnGUI()
+    {
+        if(gameOver == true)
+        {
+            GUILayout.Label("Game Over! Press any key to reset!");
+        }
+    }
+}
