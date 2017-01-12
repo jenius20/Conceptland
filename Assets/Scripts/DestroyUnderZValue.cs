c6b9b5e3e22ef9e596c430157a8de053b866e4cf
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-12 18:28:20 +0000

    Volume 2 - Added pillars + game over

---
+++ Assets/Scripts/DestroyUnderZValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyUnderZValue : MonoBehaviour
+{
+    public float destroyValue = 0.0f;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(transform.position.z <= destroyValue)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
