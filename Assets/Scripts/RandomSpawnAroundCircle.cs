00714117e8931955713ba64bed28593aa0e0bec4
Nat Bishop <nate.bishop@rocketmail.com>
2017-01-14 14:46:16 +0000

    Rotate around and cylinder

---
+++ Assets/Scripts/RandomSpawnAroundCircle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawnAroundCircle : MonoBehaviour
+{
+
+    public GameObject spawnObject;
+    public float radius = 50.0f;
+    public float minSpawnTime = 1.0f;
+    public float maxSpawnTime = 1.0f;
+
+
+    void Start()
+    {
+        Invoke("SpawnNow", Random.Range(minSpawnTime, maxSpawnTime));
+    }
+
+    void SpawnNow()
+    {
+        Vector3 center = transform.position;
+        Vector3 pos = RandomCircle(center, radius);
+        Quaternion rot = Quaternion.FromToRotation(Vector3.down, center - pos);
+        Instantiate(spawnObject, pos, rot);
+
+
+        Invoke("SpawnNow", Random.Range(minSpawnTime, maxSpawnTime));
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+
+
+}
+
+
+
---
+++ Assets/Scripts/RotateLeftAndRight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLeftAndRight : MonoBehaviour
+{
+    public Vector3 direction;
+    public Vector3 anchorPoint;
+
+
+    public float tiltPerSecond = 1.0f;
+
+ 
+        
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(anchorPoint, direction * Input.GetAxis("Horizontal"), tiltPerSecond * Time.deltaTime);
+	}
+}
